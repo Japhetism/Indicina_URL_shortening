aed75abb619ecde1b5325759367b878e6f7ac19d
Japhetism <babatundeojo30@gmail.com>
2025-05-03 23:41:56 +0100

    refactor entry

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import urlRoutes from "./routes/urlRoutes";
 import { redirectToLongUrl } from "./controllers/urlController";
+import { apiBaseUrl } from "./constants";
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.use("/api/v1", urlRoutes);
+app.use(apiBaseUrl, urlRoutes);
 
 app.get("/:url_path", redirectToLongUrl);
 
