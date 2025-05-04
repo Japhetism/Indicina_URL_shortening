a990f4ded7825d42fbb4a1b0a127e71e6566585d
Japhetism <babatundeojo30@gmail.com>
2025-05-04 15:16:50 +0100

    update document title

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UrlList from './components/URLList';
 
 const App = () => {
+
+  useEffect(() => {
+    document.title = "URL Shortening";
+  }, []);
+  
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <div className="container mx-auto">
