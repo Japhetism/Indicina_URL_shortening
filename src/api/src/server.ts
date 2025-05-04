139ad2cbb4a473d51e3696d0047ea6dd380b48ca
Japhetism <babatundeojo30@gmail.com>
2025-05-04 07:36:17 +0100

    add types

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -13,13 +13,13 @@ app.use(apiBaseUrl, urlRoutes);
 app.get("/:url_path", redirectToLongUrl);
 
 // middleware to handle non-existing routes
-app.use((req, res, next) => {
+app.use((req: Request, res: Response): void => {
   const errorMessage = `The requested URL ${req.originalUrl} was not found on this server.`;
   res.status(NOT_FOUND_HTTP_STATUS_CODE).json(ResponseHelper.error(errorMessage));
 });
 
 // global error handler for unexpected errors
-app.use((err: any, res: Response) => {
+app.use((err: any, res: Response): void => {
   const errorMessage = err.message || "An unexpected error occurred.";
   res.status(INTERNAL_SERVER_ERROR_HTTP_STATUS_CODE).json(ResponseHelper.error(errorMessage));
 })
