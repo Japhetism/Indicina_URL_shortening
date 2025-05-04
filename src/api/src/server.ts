214ec3dd77f3187ec211ca7fc611e01d5e31b1eb
Japhetism <babatundeojo30@gmail.com>
2025-05-04 07:28:00 +0100

    add middleware to handle non-exisiting routes

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import urlRoutes from "./routes/urlRoutes";
 import { redirectToLongUrl } from "./controllers/urlController";
-import { apiBaseUrl } from "./constants";
+import { apiBaseUrl, NOT_FOUND_HTTP_STATUS_CODE } from "./constants";
+import { ResponseHelper } from "./utils/responseHelper";
 
 const app = express();
 const PORT = 5000;
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use(apiBaseUrl, urlRoutes);
 
 app.get("/:url_path", redirectToLongUrl);
+
+// middleware to handle non-existing routes
+app.use((req, res, next) => {
+  const errorMessage = `The requested URL ${req.originalUrl} was not found on this server.`;
+  res.status(NOT_FOUND_HTTP_STATUS_CODE).json(ResponseHelper.error(errorMessage));
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
