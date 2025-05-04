fc4d89498a059d1d68338ce13e299bd0568aab47
Japhetism <babatundeojo30@gmail.com>
2025-05-04 15:19:43 +0100

    add navigation

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UrlList from './components/URLList';
 
 const App = () => {
-
   useEffect(() => {
     document.title = "URL Shortening";
   }, []);
-  
+
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <div className="container mx-auto">
-        <UrlList />
+    <Router>
+      <div className="min-h-screen bg-gray-100 py-8">
+        <div className="container mx-auto">
+          <Routes>
+            <Route path="/" element={<UrlList />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
